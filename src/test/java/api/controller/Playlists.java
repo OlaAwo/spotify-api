96b6fbec250c30dcf3c32a9cbd639cbc07931055
Walé <56082759+OlaAwo@users.noreply.github.com>
2022-03-29 11:11:15 +0100

    removed hard-code with constants

--- src/test/java/api/controller/Playlists.java
+++ src/test/java/api/controller/Playlists.java
@@ -5,22 +5,24 @@ import io.restassured.response.Response;
 import pojo.playlist.Playlist;
 
 import static api.TokenManager.getToken;
+import static api.constants.Playlists.PLAYLISTS;
+import static api.constants.Playlists.USERS;
 
 public class Playlists {
 
     public static Response post(Playlist requestPlaylist) {
-        return RestResource.post("/users/ola.awosile/playlists", getToken(), requestPlaylist);
+        return RestResource.post(USERS + "/ola.awosile" + PLAYLISTS, getToken(), requestPlaylist);
     }
 
     public static Response post(Playlist requestPlaylist, String token) {
-        return RestResource.post("/users/ola.awosile/playlists", token, requestPlaylist);
+        return RestResource.post(USERS + "/ola.awosile" + PLAYLISTS, token, requestPlaylist);
     }
 
     public static Response get(String playlistID) {
-        return RestResource.get("/playlists/" + playlistID, getToken());
+        return RestResource.get(PLAYLISTS + "/" + playlistID, getToken());
     }
 
     public static Response put(Playlist requestPlaylist, String playlistID) {
-        return RestResource.put("/playlists/" + playlistID, getToken(), requestPlaylist);
+        return RestResource.put(PLAYLISTS + "/" + playlistID, getToken(), requestPlaylist);
     }
 }
