1071eb4cfc0c9b4c0e717d3a7fa2d9f617bd0cab
Walé <56082759+OlaAwo@users.noreply.github.com>
2022-04-03 11:30:31 +0100

    added a class for base constants

---
+++ src/test/java/api/constants/Base.java
@@ -0,0 +1,8 @@
+package api.constants;
+
+public class Base {
+
+    public static final String BASE_PATH = "/v1";
+    public static final String API = "/api";
+    public static final String TOKEN = "/token";
+}
