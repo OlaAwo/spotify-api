aa137e22c30f20c4fda5c2d53b65ef203fa75849
Walé <56082759+OlaAwo@users.noreply.github.com>
2022-04-06 05:05:08 +0100

    added lombok annotations

--- src/test/java/pojo/error/Error.java
+++ src/test/java/pojo/error/Error.java
@@ -2,7 +2,14 @@ package pojo.error;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
 
+@Getter @Setter
+@Jacksonized
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Error {
 
